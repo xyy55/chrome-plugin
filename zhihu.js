80f6131f59598cd09a1e0871243fd088afab1141
xyy55 <1037730272@qq.com>
2019-05-14 11:46:45 +0800

    2019-5-11

--- zhihu.js
+++ zhihu.js
@@ -1,9 +1,39 @@
-$("body > div:contains('pan.baidu.com')").each(function () {
-  $(this).find("a").each(function () {
-    let e = $(this)[0].outerHTML;
-    $(this)[0].outerHTML = e.replace(/pan\.baidu\.com/gi, "####BDY####")
-  });
-  $(this).find("pan.baidu.com").each(function () {
-    console.log($(this).html())
-  })
-})
+(function () {
+  window.onload = function () {
+    // 判断必须是在知乎问题页面
+    if (!document.URL.includes('www.zhihu.com/question')) {
+      return
+    }
+    // 展开文章
+    var nodeList = document.querySelectorAll(
+      '.RichContent.is-collapsed.RichContent--unescapable'
+    )
+    nodeList.forEach(item => {
+      item.innerHTML = item.querySelector('span').innerHTML
+      item.setAttribute('class', item.getAttribute('class') + ' ztext')
+    })
+
+    // 加载全部图片（默认延迟加载）
+    var spanList = document.querySelectorAll('figure > span')
+    spanList.forEach(span => {
+      var lazyImg = span.querySelector('.VagueImage')
+      var src = lazyImg.dataset.src
+      span.innerHTML = `<img class="origin_image" src="${src}"/>`
+    })
+
+    // 删除推荐
+    removeItem(document.querySelector('.Card.RelatedReadings'))
+    removeItem(document.querySelector('.Card.HotQuestions'))
+    // 删除在 APP 打开
+    removeItem(document.querySelector('.OpenInAppButton.OpenInApp.is-shown'))
+  }
+
+  function removeItem (item) {
+    if (item == null) {
+      return
+    }
+    var parent = item.parentElement
+
+    parent.removeChild(item)
+  }
+})()
